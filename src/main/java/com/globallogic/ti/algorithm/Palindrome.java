8d35bd749d3211d6dc7162f8a2169d1f02672581
Bartosz Baranski <bartosz.baranski@globallogic.com>
2024-12-12 12:23:46 +0100

    initial commit

---
+++ src/main/java/com/globallogic/ti/algorithm/Palindrome.java
@@ -0,0 +1,13 @@
+package com.globallogic.ti.algorithm;
+
+public class Palindrome {
+
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("kajak"));
+    }
+
+    private static boolean isPalindrome(String candidate) {
+        return false;
+    }
+}
